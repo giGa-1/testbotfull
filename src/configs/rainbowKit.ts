dd2749b89e0af792772f9c505a722756adb158f6
giGa-1 <1gledpobgornuk@gmail.com>
2024-10-22 16:27:59 +0200

    fckaksdaksd

--- src/configs/rainbowKit.ts
+++ src/configs/rainbowKit.ts
@@ -2,7 +2,7 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { http } from 'wagmi';
 import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains';
 
-const PROJECT_ID = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || '';
+const PROJECT_ID = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || 'ed422d784f4873b2bfd9a6e98f35b713';
 
 export const rainbowConfig = getDefaultConfig({
     appName: 'My RainbowKit App',
